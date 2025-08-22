2febbccbfe56b7177d03abf8c540c56947c30fed
JyojiTenguam <j.y.oji@hotmail.com>
2025-08-22 18:31:23 -0300

    feat: adiciona tela de carregamento (LoadingScreen)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import './App.css'
+import { LoadingScreen } from './components/LoadingScreen'
+import "./index.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
