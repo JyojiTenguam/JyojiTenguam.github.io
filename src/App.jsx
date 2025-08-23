753ad9557644d2618fdd910a06b023f42f5ae3ab
JyojiTenguam <j.y.oji@hotmail.com>
2025-08-23 04:28:14 -0300

    feat: adiciona Navbar e menu mobile (MobileMenu)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
+import { Navbar } from './components/Navbar';
 import "./index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
-
+  const [menuOpen, setMenuOpen] = useState(false);
+  
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
@@ -13,6 +15,7 @@ function App() {
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
