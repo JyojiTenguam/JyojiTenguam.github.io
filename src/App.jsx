5e9b74f82e1dd7a1089e01b102745d15c4d993ec
JyojiTenguam <j.y.oji@hotmail.com>
2025-08-23 04:46:14 -0300

    feat: adiciona seção Home ao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
 import { Navbar } from './components/Navbar';
 import "./index.css";
+import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/sections/Home';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-  
+
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
@@ -16,6 +18,8 @@ function App() {
         } bg-black text-gray-100`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   );
