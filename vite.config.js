95c3c9a927b90755a29027dea29fb2a264416df8
JyojiTenguam <j.y.oji@hotmail.com>
2025-08-28 17:28:56 -0300

    chore: configura base e plugins no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/jyoji-portfolio",
+  base: "/jyoji-portfolio/",
 }); 
